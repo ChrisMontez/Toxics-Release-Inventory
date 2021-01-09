92ff1bf4e7a2962d170fbf5df5da0ba1503cd01f
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2021-01-09 03:54:59 -0600

    --

--- app.py
+++ app.py
@@ -1,51 +1,82 @@
-# from dash.react import dash
-
-# my_app = Dash('my app')
-
-# from dash_componentsw import h1, Plotly
-
-# my_app.layout = h1('Hello World')
-
-# my_app.server.run(debug=True)
-
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+import numpy as np
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
+
+# Load Data
 df = pd.read_csv('tri_2019_us.csv')
-styrene  = df[(df['CHEMICAL'] == 'STYRENE')]
+
+available_indicators = df['CHEMICAL'].unique()
+
+#Display
+# fig = px.density_mapbox(df, lat='LATITUDE', lon ='LONGITUDE', z='STACK AIR', radius=10,  hover_data=["FACILITY NAME", "STACK AIR", "UNIT OF MEASURE"],
+# #                         center=dict(lat=0, lon=0), 
+#                         zoom=2,
+#                         # animation_frame="CHEMICAL",
+#                         labels={"STACK AIR": "Emissions (lbs)"},
+#                         mapbox_style="stamen-terrain")
 
 
-fig = px.density_mapbox(styrene, lat='LATITUDE', lon ='LONGITUDE', z='STACK AIR', radius=10,  hover_data=["FACILITY NAME", "STACK AIR", "UNIT OF MEASURE"],
-                        center=dict(lat=0, lon=0), zoom=0,
-                        labels={"STACK AIR": "Emissions (lbs)"},
-                        mapbox_style="stamen-terrain")
+# app.layout = html.Div(children=[
+#     html.H1(children='Testing One Two Three'),
+#     html.Div(children='''
+#         test test test.
+#     '''),
+#     dcc.Graph(
+#         id='example-graph',
+#         figure=fig
+#     )
+# ])
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+app.layout = html.Div([
+	html.Div([
+	    html.Label('Chemicals'),
+		dcc.Dropdown(
+		id='chemical',
+		options=[{'label': i, 'value': i} for i in available_indicators],
+		value='STYRENE'
+		),
+		dcc.Graph(id='indicator-graphic')
+	])
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
 ])
+
+# app.callback(
+# 	 Output('indicator-graphic', 'figure'),
+# 	 Input('chemical', 'value')
+# 	 )
+
+
+#     Output('example-graph', 'figure'),
+#     Input('chemical', 'value'))
+
+# def update_graph(chemical_name, chemical_value):
+
+# 	dff = df[df['CHEMICAL'] == chemical_value]
+
+# 	fig = px.density_mapbox(df, 
+# 		lat=dff[dff['Indicator Name'] == chemical_name]['LATITUDE'], 
+# 		lon=dff[dff['Indicator Name'] == chemical_name]['LONGITUDE'], 
+# 		z=dff[dff['Indicator Name'] == chemical_name]['STACK AIR'], 
+# 		radius=10,  
+# 		hover_data=["FACILITY NAME", "STACK AIR", "UNIT OF MEASURE"],
+# 	    zoom=2,
+# 	    labels={"STACK AIR": "Emissions (lbs)"},
+# 	    mapbox_style="stamen-terrain"
+# 	 )
+
+# 	return fig
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
