aa5dd25c78d276720b4d9446ff3348fbebc70571
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2021-01-09 08:27:14 -0600

    Added Drop down for analysis of various chemicals

--- app.py
+++ app.py
@@ -16,25 +16,6 @@ df = pd.read_csv('tri_2019_us.csv')
 
 available_indicators = df['CHEMICAL'].unique()
 
-#Display
-# fig = px.density_mapbox(df, lat='LATITUDE', lon ='LONGITUDE', z='STACK AIR', radius=10,  hover_data=["FACILITY NAME", "STACK AIR", "UNIT OF MEASURE"],
-# #                         center=dict(lat=0, lon=0), 
-#                         zoom=2,
-#                         # animation_frame="CHEMICAL",
-#                         labels={"STACK AIR": "Emissions (lbs)"},
-#                         mapbox_style="stamen-terrain")
-
-
-# app.layout = html.Div(children=[
-#     html.H1(children='Testing One Two Three'),
-#     html.Div(children='''
-#         test test test.
-#     '''),
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-# ])
 
 app.layout = html.Div([
 	html.Div([
@@ -42,6 +23,7 @@ app.layout = html.Div([
 		    # html.Label('Chemicals'),
 			dcc.Dropdown(
 			id='selected_chemical',
+			value = 'STYRENE',
 			options=[{'label': i, 'value': i} for i in available_indicators])
 		])
 	]),
@@ -55,10 +37,6 @@ app.layout = html.Div([
 
 @app.callback(dash.dependencies.Output("graphic","figure"),
 	[dash.dependencies.Input("selected_chemical","value")])
-# app.callback(
-# 	[Output('graphic', 'figure')],
-# 	[Input('selected_chemical', 'value')])
-
 
 
 def update_graph(input_value):
@@ -74,17 +52,6 @@ def update_graph(input_value):
                         labels={"STACK AIR": "Emissions (lbs)"},
                         mapbox_style="stamen-terrain")
 
-	# fig = px.density_mapbox(dff, 
-	# 	lat=df[df['Indicator Name'] == input_value]['LATITUDE'], 
-	# 	lon=df[df['Indicator Name'] == input_value]['LONGITUDE'], 
-	# 	z=df[df['Indicator Name'] == input_value]['STACK AIR'], 
-	# 	radius=10,  
-	# 	hover_data=["FACILITY NAME", "STACK AIR", "UNIT OF MEASURE"],
-	#     zoom=2,
-	#     labels={"STACK AIR": "Emissions (lbs)"},
-
-	#     mapbox_style="stamen-terrain"
-	#  )
 
 	return fig
 
